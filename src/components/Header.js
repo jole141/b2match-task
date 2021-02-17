64607d0623bc5d899b3cff4f88f01beab063248a
jjurenic <josip.jurenic12@gmail.com>
2021-02-17 17:40:39 +0100

    Fix logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,11 @@
 import React from "react";
 import HeaderDiv from "../styled-components/HeaderDiv";
+import logoImg from "../images/b.svg";
 
 const Header = () => {
   return (
     <HeaderDiv>
-      <img
-        src="https://www.flaticon.com/svg/vstatic/svg/3522/3522266.svg?token=exp=1613128697~hmac=88d45972cec01862c8d6e0768a2c54eb"
-        alt="Letter B"
-      />
+      <img src={logoImg} alt="Letter B" />
     </HeaderDiv>
   );
 };
